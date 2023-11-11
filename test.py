4c892fe3b993f0659f4ed25bc0b1da3254e769f5
Somasekhar Gollapalli <98102921+Sekharbabu12@users.noreply.github.com>
2023-11-11 10:59:22 +0530

    Update test.py
    updated test.py file

--- test.py
+++ test.py
@@ -1,15 +1,17 @@
 import pytest
-from app import app
-
+from app import app as tested_app
 
 @pytest.fixture
-def client():
-    app.config['TESTING'] = True
-    with app.test_client() as client:
-        yield client
+def app():
+    tested_app.config['TESTING'] = True
+    yield tested_app
 
+@pytest.fixture
+def client(app):
+    return app.test_client()
 
 def test_app_is_working(client):
     response = client.get('/')
     assert response.status_code == 200
     assert b"Hello World!" in response.data
+
